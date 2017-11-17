4a3fe30483b63bade976f247b9b0819880ce7c8d
epa <epa@localhost.localdomain>
2017-11-17 10:52:41 +0000

    loadtest

---
+++ week8/loadtestWrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# invoke the load test with appropriate parameters
+# sleep for the duration of the load test T
+# pkill the load test
+
+./loadtest $1 &
+
+sleep $2
+
+pkill loadtest
